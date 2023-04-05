2a7f3a9e0e0b8e4dfc7c7c260947dd64129aa552
juicydolphin <dolphin.krsk@gmail.com>
2023-04-05 23:46:44 +0700

    Second

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const { Pool } = require('pg');
 require('dotenv').config();
+const swaggerUi = require('swagger-ui-express');
+const specs = require('./swaggerSpecs');
 
 const app = express();
 
@@ -12,15 +14,72 @@ const pool = new Pool({
     port: process.env.DB_PORT,
 });
 
+// Добавьте эти строки перед app.get('/', async (req, res) => {...
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+
+/**
+ * @swagger
+ *
+ * /:
+ *   get:
+ *     summary: Получить список пользователей.
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: Список пользователей.
+ */
 app.get('/', async (req, res) => {
     try {
-        const { rows } = await pool.query('SELECT * FROM my_table');
+        const { rows } = await pool.query('SELECT * FROM users');
         res.json(rows);
     } catch (err) {
         console.error(err);
         res.status(500).send('Internal server error');
     }
 });
+
+/**
+ * @swagger
+ *
+ * /create_users_table:
+ *   get:
+ *     summary: Создать таблицу пользователей и заполнить ее.
+ *     produces:
+ *       - text/plain
+ *     responses:
+ *       200:
+ *         description: Таблица создана и заполнена.
+ *       500:
+ *         description: Ошибка при создании таблицы.
+ */
+app.get('/create_users_table', async (req, res) => {
+    try {
+        const query = `
+      CREATE TABLE users (
+        id SERIAL PRIMARY KEY,
+        email VARCHAR(255) NOT NULL,
+        password VARCHAR(255) NOT NULL
+      )
+    `;
+        await pool.query(query);
+        // Заполнение таблицы тремя случайными записями
+        for (let i = 0; i < 3; i++) {
+            const email = `user${i}@example.com`;
+            const password = `password${i}`;
+            const insertQuery = `
+        INSERT INTO users (email, password)
+        VALUES ($1, $2)
+      `;
+            await pool.query(insertQuery, [email, password]);
+        }
+        res.send('Таблица создана и заполнена');
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Ошибка при создании таблицы');
+    }
+});
+
 
 app.listen(3000, () => {
     console.log('Server started on port 3000');
---
+++ swaggerSpecs.js
@@ -0,0 +1,16 @@
+const swaggerJsdoc = require('swagger-jsdoc');
+
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'My API',
+            version: '1.0.0',
+        },
+    },
+    apis: ['index.js'],
+};
+
+const specs = swaggerJsdoc(options);
+
+module.exports = specs;
